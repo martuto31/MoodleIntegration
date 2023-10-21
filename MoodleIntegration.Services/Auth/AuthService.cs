63d966d41d56aef421b01a2959b5be317f8b2c84
Martin Petrov <martuto31@gmail.com>
2023-10-21 17:57:01 +0300

    Added documentation comments.

--- MoodleIntegration.Services/Auth/AuthService.cs
+++ MoodleIntegration.Services/Auth/AuthService.cs
@@ -7,7 +7,7 @@ namespace MoodleIntegration.Services.Auth
     {
         public async Task<HttpResponseMessage> GetTokenAsync(HttpClient client, string code)
         {
-            // Create the POST request content with the parameters
+            // Creates the POST request content for getting the token from moodle
             var content = new FormUrlEncodedContent(new[]
             {
                     new KeyValuePair<string, string>("code", code),
@@ -25,12 +25,15 @@ namespace MoodleIntegration.Services.Auth
 
         public async Task<HttpResponseMessage> GetUserInfoAsync(HttpClient client, MoodleTokenDTO moodleToken)
         {
+            // Creates the POST request content for getting the user info from moodle
             var access_token = new FormUrlEncodedContent(new[]
             {
                 new KeyValuePair<string, string>("access_token", moodleToken.access_token)
             });
 
+            // Sends the POST request to the Moodle user info endpoint
             var userInfo = await client.PostAsync(MoodleAuthConstants.User_Info_Url, access_token);
+
             return userInfo;
         }
     }
--- MoodleIntegration/Controllers/UserController.cs
+++ MoodleIntegration/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MoodleIntegration.Services.Auth;
 using MoodleIntegration.Shared.DTO;
+using System.ComponentModel.Design;
 using System.Text.Json;
 
 namespace MoodleIntegration.Controllers
@@ -17,22 +18,37 @@ namespace MoodleIntegration.Controllers
         }
 
         [HttpGet("Callback")]
-        public async Task<IActionResult> HandleMoodleCallback(string code)
+        public async Task<IActionResult> HandleMoodleAuthCallbackAsync(string code)
         {
             using (var client = new HttpClient())
             {
-                var response = await _authService.GetTokenAsync(client, code);
+                // Sends request to moodle for the authentication token
+                var AuthResponse = await _authService.GetTokenAsync(client, code);
 
-                if (response.IsSuccessStatusCode)
+                if (AuthResponse.IsSuccessStatusCode)
                 {
-                    var AuthResponseContent = await response.Content.ReadAsStringAsync();
+                    // Deserialize the authentication token
+                    var AuthResponseContent = await AuthResponse.Content.ReadAsStringAsync();
                     MoodleTokenDTO moodleToken = JsonSerializer.Deserialize<MoodleTokenDTO>(AuthResponseContent);
 
-                    var UserInfoResponseContent = await _authService.GetUserInfoAsync(client, moodleToken);
+                    // Sends request to moodle for the authenticated user's info
+                    var UserInfoResponse = await _authService.GetUserInfoAsync(client, moodleToken);
+                    if(UserInfoResponse.IsSuccessStatusCode)
+                    {
+                        // Deserialise the user info
+                        var UserInfoResponseContent = await UserInfoResponse.Content.ReadAsStringAsync();
+                        UserInfoDTO userInfo = JsonSerializer.Deserialize<UserInfoDTO>(UserInfoResponseContent);
+
+
+                    }
+                    else
+                    {
+                        return BadRequest(UserInfoResponse);
+                    }
                 }
                 else
                 {
-                    return BadRequest(response);
+                    return BadRequest(AuthResponse);
                 }
             }
 
